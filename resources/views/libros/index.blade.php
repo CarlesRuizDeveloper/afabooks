8a2a40a3fec2da4a7a4ddfc6e18c2ca1610635a4
CarlesDeveloperPHP <crmfeina@gmail.com>
2023-12-29 11:24:47 +0100

    Creada la base con taiwind de la Landing Page

--- resources/views/libros/index.blade.php
+++ resources/views/libros/index.blade.php
@@ -3,60 +3,65 @@
 @section('title', 'Home')
 
 @section('content')
-    <h1>pagina principal libros</h1>   
-    
-    <a href="{{route('libros.create')}}" >Donar libro</a>
-    <ul>
-        @foreach ($libros as $libro)
-            <li>
-                <a href="{{route('libros.show', $libro->id)}}" >{{$libro->id}}</a>
-            </li>
-        @endforeach
-    </ul> 
-    <style>
-        .slider-container {
-            width: 80%; /* Ajusta el ancho del slider según tu diseño */
-        }
-    
-        .slider-wrapper {
-            transition: transform 0.5s ease-in-out;
-        }
-    
-        .slide {
-            min-width: 250px; /* Ajusta el ancho de cada slide según tu diseño */
-            margin-right: 20px; /* Espaciado entre slides */
-        }
-    </style>
-    <div class="flex items-center justify-center h-screen">
-        <div class="slider-container overflow-hidden">
-            <div class="slider-wrapper flex transition-transform duration-300 ease-in-out">
-                @foreach ($libros as $libro)
-                    <div class="slide p-4">
-                        <h3 class="text-lg font-semibold">{{ $libro->codigo }}</h3>
-                        <p class="text-sm text-gray-600">{{ $libro->codigo }}</p>
-                        <a href="{{route('libros.show', $libro->id)}}" >{{$libro->id}}</a>
-                    </div>
-                @endforeach
-            </div>
-        </div>
+<div class="hidden md:block bg-green-200 p-6 rounded-lg shadow-md mx-auto my-2 text-center w-2/3 mt-4 mb-4">
+    <p class="text-gray-700">
+        Gracias por unirte a nuestra campaña para reciclar libros. Cada libro que reciclas contribuye a construir un futuro más sostenible.
+        <br>
+        AFA Vedruna Gràcia
+    </p>
+</div>
+
+
+    <div class="md:hidden bg-green-200 p-6 rounded-lg shadow-md mx-4 my-2 text-center">
+        <p class="text-gray-700">
+            Gracias por unirte a nuestra campaña para reciclar libros. Cada libro que reciclas contribuye a construir un futuro más sostenible.
+            <br>
+            AFA Vedruna Gràcia
+        </p>
     </div>
-    {{$libros->links()}}
+
+    <div class="hidden md:block">
+        <!-- Visible solo en pantallas medianas y grandes -->
+        <a href="{{ route('libros.create') }}" class="block mx-auto mb-4 p-2 bg-green-700 rounded-md text-center text-white md:w-1/4">
+            Donar libros
+        </a>
+    </div>
+    <a href="{{ route('libros.create') }}" class="block mx-auto mb-4 p-2 bg-green-700 rounded-md text-center text-white md:hidden w-1/3">
+        Donar libros
+    </a>
+    <h1 class="mb-2 font-bold text-xl lg:text-2xl xl:text-3xl">Libros de texto</h1> 
+
+    <div class="flex flex-nowrap overflow-x-scroll">        
+        <a href="{{ route('libros.create') }}" class="block mx-auto mb-4 p-2 bg-green-700 rounded-md text-center text-white md:w-1/4 sm:w-full sm:max-w-xs">
+            Ver todos
+        </a>
+    
+        @foreach ($libros->take(8) as $libro)
+            <div class="flex-none bg-orange-300 mx-2 sm:mx-4 md:mx-6 lg:mx-8 xl:mx-10 p-4 w-32 text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl max-h-full">
+                <a href="{{ route('libros.show', $libro->codigo) }}" class="block mb-2">{{ $libro->id }}</a>
+                <a href="{{ route('libros.show', $libro->codigo) }}" class="block mb-2">{{ $libro->codigo }}</a>
+                <a href="{{ route('libros.show', $libro->codigo) }}" class="block mb-2">{{ $libro->id }}</a>
+                <a href="{{ route('libros.show', $libro->codigo) }}" class="block mb-2">{{ $libro->codigo }}</a>
+            </div>
+        @endforeach
+    </div>
+    <h1 class="mb-2 mt-3 font-bold text-xl lg:text-2xl xl:text-3xl">Libros de lectura</h1> 
+
+    <div class="flex flex-nowrap overflow-x-scroll">        
+        <a href="{{ route('libros.create') }}" class="block mx-auto mb-4 p-2 bg-green-700 rounded-md text-center text-white md:w-1/4 sm:w-full sm:max-w-xs">
+            Ver todos
+        </a>
+    
+        @foreach ($libros->take(8) as $libro)
+            <div class="flex-none bg-orange-300 mx-2 sm:mx-4 md:mx-6 lg:mx-8 xl:mx-10 p-4 w-32 text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl max-h-full">
+                <a href="{{ route('libros.show', $libro->codigo) }}" class="block mb-2">{{ $libro->id }}</a>
+                <a href="{{ route('libros.show', $libro->codigo) }}" class="block mb-2">{{ $libro->codigo }}</a>
+                <a href="{{ route('libros.show', $libro->codigo) }}" class="block mb-2">{{ $libro->id }}</a>
+                <a href="{{ route('libros.show', $libro->codigo) }}" class="block mb-2">{{ $libro->codigo }}</a>
+            </div>
+        @endforeach
+    </div>
+    
+
+    
 @endsection
-
-<script>
-    const slider = document.querySelector('.slider-wrapper');
-    let translateValue = 0;
-
-    function nextSlide() {
-        translateValue -= 270; // Ajusta el valor según el ancho de cada slide + margen
-        slider.style.transform = `translateX(${translateValue}px)`;
-    }
-
-    function prevSlide() {
-        translateValue += 270; // Ajusta el valor según el ancho de cada slide + margen
-        slider.style.transform = `translateX(${translateValue}px)`;
-    }
-</script>
-
-
-
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LibroController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,8 +18,22 @@ use App\Http\Controllers\LibroController;
 
 Route::get('/',HomeController::class);
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/', [ LibroController::class , 'index' ])->name('libros.index');
+
 Route::get('libros', [ LibroController::class , 'index' ])->name('libros.index');
 
-Route::get('libros/create',[ LibroController::class , 'create'])->name('libros.create');
+Route::get('libros/create',[ LibroController::class , 'create'])->middleware(['auth', 'verified'])->name('libros.create');
 
-Route::get('libros/{id}', [LibroController::class , 'show'])->name('libros.show');
+Route::get('libros/{id}', [LibroController::class , 'show'])->middleware(['auth', 'verified'])->name('libros.show');
+
+require __DIR__.'/auth.php';
